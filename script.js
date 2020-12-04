ca29d582dc8069e5190d0e5838e0113e02616747
TaylerOndersma <tayleron@umich.edu>
2020-12-03 20:44:15 -0500

    Cleaning up logic and added comments

--- script.js
+++ script.js
@@ -1,62 +1,42 @@
 let rainFallInput = prompt("How many inches of rain fell?");
-let aLine = "";
+// store user's rain input and return "*"s equal to their input in the console
+let aLine = "";                 
 let rain = rainFallInput
-
-const DEF_CROP_YIELD = 50;
-const HIGH_RAIN = 20;
-const DEC_HIGH_RAIN = .9;
-const LOW_RAIN = 10;
-const DEC_LOW_RAIN = .8;
-const MULT_REG_FERT = 1.1;
-const MULT_PREM_FERT = 1.15;
-
 while(rain > 0){
     aLine += "*";
     rain --;
 }
 console.log(aLine);
 
+// define constants
+const DEF_CROP_YIELD = 50; 
+const HIGH_RAIN = 20; 
+const DEC_HIGH_RAIN = .9; 
+const LOW_RAIN = 10;
+const DEC_LOW_RAIN = .8;
+const MULT_REG_FERT = 1.1;
+const MULT_PREM_FERT = 1.15;
+
+// find crop yield for too much rain
+let cropYield = 0;
 if(rainFallInput >= HIGH_RAIN){
-    let fertilizer = prompt("Did you use fertilizer?");
-    let cropYield = DEF_CROP_YIELD*DEC_HIGH_RAIN;
-    if(fertilizer === "yes"){
-        let typeFert = prompt("Did you use premium or regular fertilizer?");
-        if(typeFert === "regular"){
-            let updatedCropYield = cropYield*MULT_REG_FERT;
-            console.log("The yield should be " + updatedCropYield + " bushels per acre.");
-    }
-        if(typeFert === "premium"){
-            let updatedCropYield = cropYield*MULT_PREM_FERT;
-            console.log("The yield should be " + updatedCropYield + " bushels per acre.");
-        }
-}
-    if(fertilizer === "no"){
-        console.log("the yield should be " + cropYield + " bushels per acre.");
-    }
+    cropYield = DEF_CROP_YIELD*DEC_HIGH_RAIN;
 }
 
-if(rainFallInput <= LOW_RAIN){
-    let fertilizer = prompt("Did you use fertilizer?");
-    let cropYield = DEF_CROP_YIELD*DEC_LOW_RAIN;
-    if(fertilizer === "yes"){
-        let typeFert = prompt("Did you use premium or regular fertilizer?");
-        if(typeFert === "regular"){
-            let updatedCropYield = cropYield*MULT_REG_FERT
-            console.log("The yield should be " + updatedCropYield + " bushels per acre.");
-    }
-        if(typeFert === "premium"){
-            let updatedCropYield = cropYield*MULT_PREM_FERT
-            console.log("The yield should be " + updatedCropYield + " bushels per acre.");
-        }
-}
-    if(fertilizer === "no"){
-        console.log("the yield should be " + cropYield + " bushels per acre.");
-    }
+// find crop yield for too little rain
+else if(rainFallInput <= LOW_RAIN){
+    cropYield = DEF_CROP_YIELD*DEC_LOW_RAIN;
 }
 
-if(rainFallInput > LOW_RAIN && rainFallInput < HIGH_RAIN){
+// find crop yield for ideal rain 
+else{
+    cropYield = DEF_CROP_YIELD;
+}
+findCropYield (cropYield);
+
+// function for handling user's input about fertilizer
+function findCropYield(cropYield) {
     let fertilizer = prompt("Did you use fertilizer?");
-    let cropYield = DEF_CROP_YIELD;
     if(fertilizer === "yes"){
         let typeFert = prompt("Did you use premium or regular fertilizer?");
         if(typeFert === "regular"){
